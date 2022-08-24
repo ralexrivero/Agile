cfccba4942cb9afe4ed7ca59a874b13bca25eb1d
ralexrivero <ralexrivero@gmail.com>
2022-08-24 02:24:26 -0300

    first roman numeral and variable name improved

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Agile
 Agile improvement
+
+The goal is to keep as little code as possible to have the least number of bugs and achieve the greatest technical agility and business agility.
+
+## Conversion
+
+- I = 1
+- V = 5
+- X = 10
+- L = 50
+- C = 100
+- D = 500
+- M = 1000
+
+## Refactorings
+
+- Rename Variable: change variable's name so it better reveals its purpose
+- 
--- src/test/java/com/agile/kata/RomanTests.java
+++ src/test/java/com/agile/kata/RomanTests.java
@@ -1,4 +1,24 @@
 package com.agile.kata;
 
+import jdk.jfr.StackTrace;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class RomanTests {
+    @Test
+    public void junitWorks() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void convertSingleDigitRoman() {
+        int arabic = convert("I");
+        assertEquals(1, arabic);
+    }
+
+    private int convert(String romanNumeral) {
+        return 1;
+    }
 }
