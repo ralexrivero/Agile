df2da09ca94019b052c01d679ed1e164fb29dd02
ralexrivero <ralexrivero@gmail.com>
2022-08-25 04:17:23 -0300

    extract method for single digit

--- src/main/java/com/agile/kata/Roman.java
+++ src/main/java/com/agile/kata/Roman.java
@@ -3,21 +3,7 @@ package com.agile.kata;
 public class Roman {
     static int convert(String romanNumeral) {
         if (romanNumeral.length() == 1) {
-            if (romanNumeral.equals("I")) {
-                return 1;
-            } else if (romanNumeral.equals("V")){
-                return 5;
-            } else if (romanNumeral.equals("X")) {
-                return 10;
-            } else if (romanNumeral.equals("L")) {
-                return 50;
-            } else if (romanNumeral.equals("C")) {
-                return 100;
-            } else if (romanNumeral.equals("D")) {
-                return 500;
-            } else {
-                return 1000;
-            }
+            return convertSingleDigit(romanNumeral);
         }
         if (romanNumeral.equals("II")) {
             return 2;
@@ -25,6 +11,24 @@ public class Roman {
             return 3;
         } else {
             return 6;
+        }
+    }
+
+    private static int convertSingleDigit(String romanNumeral) {
+        if (romanNumeral.equals("I")) {
+            return 1;
+        } else if (romanNumeral.equals("V")){
+            return 5;
+        } else if (romanNumeral.equals("X")) {
+            return 10;
+        } else if (romanNumeral.equals("L")) {
+            return 50;
+        } else if (romanNumeral.equals("C")) {
+            return 100;
+        } else if (romanNumeral.equals("D")) {
+            return 500;
+        } else {
+            return 1000;
         }
     }
 }
